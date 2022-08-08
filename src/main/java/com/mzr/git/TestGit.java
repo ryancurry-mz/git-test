f4c7dc57cf17de9ce1963fb0fef49e4878dea03b
ryancurry <hanma0426@163.com>
2022-08-08 18:39:13 +0800

    first commit

---
+++ src/main/java/com/mzr/git/TestGit.java
@@ -0,0 +1,13 @@
+package com.mzr.git;
+
+/**
+ * @author: ryancurry
+ * @Date: 2022-08-08 18:37
+ * @Description:
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!!");
+    }
+}
