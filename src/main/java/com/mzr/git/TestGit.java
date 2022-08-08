768097b48d6eab6fc0ad926271730d04e05782cf
ryancurry <hanma0426@163.com>
2022-08-08 19:24:44 +0800

    push commit

--- src/main/java/com/mzr/git/TestGit.java
+++ src/main/java/com/mzr/git/TestGit.java
@@ -14,5 +14,6 @@ public class TestGit {
         System.out.println("hello git444!!");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
