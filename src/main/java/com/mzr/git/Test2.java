56885e006ec9610084e8c7d33b820c5aca8441a8
ryancurry <hanma0426@163.com>
2022-08-08 19:19:27 +0800

    test2 test commit

---
+++ src/main/java/com/mzr/git/Test2.java
@@ -0,0 +1,16 @@
+package com.mzr.git;
+
+import org.junit.Test;
+
+/**
+ * @author: ryancurry
+ * @Date: 2022-08-08 18:41
+ * @Description:
+ */
+public class Test2 {
+
+    @Test
+    public void test(){
+        System.out.println("测试测试");
+    }
+}
