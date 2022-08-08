5871acf0635f854f1e923e1592a56e6a41f2be20
ryancurry <hanma0426@163.com>
2022-08-08 18:57:24 +0800

    hot-fix commit

--- src/main/java/com/mzr/git/TestGit.java
+++ src/main/java/com/mzr/git/TestGit.java
@@ -11,5 +11,6 @@ public class TestGit {
         System.out.println("hello git!!");
         System.out.println("hello git222!!");
         System.out.println("hello git333!!");
+        System.out.println("hello git444!!");
     }
 }
